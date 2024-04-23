df4e59425364e9ba5963053b382f6843f6fac6ba
horilla <khjin1197@gmail.com>
2024-04-24 01:53:13 +0900

    init project

---
+++ src/main/kotlin/org/khjin/hhplusecommerce/HhplusEcommerceApplication.kt
@@ -0,0 +1,11 @@
+package org.khjin.hhplusecommerce
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class HhplusEcommerceApplication
+
+fun main(args: Array<String>) {
+    runApplication<HhplusEcommerceApplication>(*args)
+}
