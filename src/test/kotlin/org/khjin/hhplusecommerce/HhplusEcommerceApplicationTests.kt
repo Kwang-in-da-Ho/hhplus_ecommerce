13f883af49101c12b353f9ee2b3d945f429d8c9f
horilla <khjin1197@gmail.com>
2024-04-24 01:53:29 +0900

    add test main class

---
+++ src/test/kotlin/org/khjin/hhplusecommerce/HhplusEcommerceApplicationTests.kt
@@ -0,0 +1,13 @@
+package org.khjin.hhplusecommerce
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class HhplusEcommerceApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
